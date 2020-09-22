2326d9f92fc067ab38553c7ff232f87c0efcc226
Rolando Anton <rolando@anton.sh>
2020-09-22 20:03:31 +0000

    fixing variables

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "dc" {
   description = "Name of the datacenter you want to create the vApp."
 }
 
+variable "vswitch" { 
+  description = "Define the Dsitributed Virtual Switch to attach the Port Group"
+}
+
 variable "portgroup-name" {
   description = "The name of the Port Group to create"
 }
